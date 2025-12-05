3e106d365c3b3465f721cce674e450b6c95e7023
ZeeshanLive <41287858+ZeeshanLive@users.noreply.github.com>
2025-12-06 00:47:27 +0530

    Update variables.tf

--- terraform-module/ecr/variables.tf
+++ terraform-module/ecr/variables.tf
@@ -1,8 +1,11 @@
-
-
 variable "repositories" {
   type = map(object({
-    lifecycle_policy = optional(string)
+    name                 = string
+    encryption_type      = string
+    kms_key              = string
+    image_tag_mutability = string
+    scan_on_push         = bool
+    tags                 = any
+    lifecycle_policy     = string
   }))
 }
-
