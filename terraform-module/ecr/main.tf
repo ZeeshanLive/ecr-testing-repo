10b47ef6b860d39bc70cdfc664ebfb0290490ee6
ZeeshanLive <41287858+ZeeshanLive@users.noreply.github.com>
2025-12-25 04:28:08 +0530

    Update main.tf

--- terraform-module/ecr/main.tf
+++ terraform-module/ecr/main.tf
@@ -1,25 +1,14 @@
 resource "aws_ecr_repository" "this" {
-  for_each = var.repositories
-
-  name                 = each.value.name
-  image_tag_mutability = each.value.image_tag_mutability
-
-  encryption_configuration {
-    encryption_type = each.value.encryption_type
-    kms_key         = lookup(each.value, "kms_key", null)
-  }
+  name                 = var.name
+  image_tag_mutability = var.image_tag_mutability
 
   image_scanning_configuration {
-    scan_on_push = each.value.scan_on_push
+    scan_on_push = var.scan_on_push
   }
-
-  tags = each.value.tags
 }
 
-# Manage lifecycle policies
 resource "aws_ecr_lifecycle_policy" "this" {
-  for_each = { for k, v in var.repositories : k => v if contains(keys(v), "lifecycle_policy") && v.lifecycle_policy != "" }
-
-  repository = each.value.name
-  policy     = each.value.lifecycle_policy
+  count      = var.lifecycle_policy != null ? 1 : 0
+  repository = aws_ecr_repository.this.name
+  policy     = var.lifecycle_policy
 }
